81acf6bcec0aaf916b65d6bcd24c117f89585bda
Santicm23 <santicastrom@outlook.com>
2023-09-14 19:50:20 -0500

    controlador usuario

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -1,12 +1,69 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Usuario;
 import com.example.demo.model.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @GetMapping(value = "/usuarios", produces = "application/json")
+    public Iterable<Usuario> getUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping(value = "/usuario/{uid}", produces = "application/json")
+    public Usuario getUsuarioById(@PathVariable Long uid) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(uid);
+
+        if (usuarioOptional.isPresent()) {
+            return usuarioOptional.get();
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Usuario no encontrado");
+        }
+    }
+
+    @PostMapping(value = "/usuario", produces = "application/json")
+    public Usuario createUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @PutMapping(value = "/usuario/{uid}", produces = "application/json")
+    public Usuario updateUsuario(@PathVariable Long uid, Usuario usuario) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(uid);
+
+        if (usuarioOptional.isPresent()) {
+            Usuario usuarioTemp = usuarioOptional.get();
+            usuarioTemp.setNombre(usuario.getNombre());
+            usuarioTemp.setCorreo(usuario.getCorreo());
+            usuarioTemp.setContrasena(usuario.getContrasena());
+            usuarioTemp.setRol(usuario.getRol());
+
+            return usuarioRepository.save(usuarioTemp);
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Usuario no encontrado");
+        }
+    }
+
+    @DeleteMapping(value = "/usuario/{uid}", produces = "application/json")
+    public void deleteUsuario(@PathVariable Long uid) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(uid);
+
+        if (usuarioOptional.isPresent()) {
+            usuarioRepository.delete(usuarioOptional.get());
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Usuario no encontrado");
+        }
+    }
 }
