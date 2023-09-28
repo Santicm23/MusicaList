f613ed967ce0de9232e95b55945d072d68d26ea5
Santicm23 <santicastrom@outlook.com>
2023-09-28 11:38:59 -0500

    usuario test corregido

---
+++ src/test/java/com/example/demo/UsuarioTest.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.repostories.UsuarioRepository;
+
+@SpringBootTest
+@Transactional
+public class UsuarioTest {
+
+    @Autowired
+    protected UsuarioRepository usuarioRepository;
+
+    @Test
+    void obtenerUsuarios() {
+
+    }
+
+    @Test
+    void obtenerUsuario() {
+
+    }
+
+    @Test
+    void crearUsuario() {
+
+    }
+
+    @Test
+    void actualizarUsuario() {
+
+    }
+
+    @Test
+    void eliminarUsuario() {
+
+    }
+}
+
